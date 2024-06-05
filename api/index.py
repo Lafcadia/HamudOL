53e49731454893817950c96d229c40475494761e
ChuishenX <chuishenx@oblivionocean.top>
2024-06-05 20:57:23 +0800

    Changes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/background_process_test')
+def background_process_test():
+    print ("Hello")
+    return ("nothing")
