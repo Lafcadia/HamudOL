fec5e2532f2ac3db50f509620de5160852acf398
ChuishenX <chuishenx@oblivionocean.top>
2024-06-29 14:30:11 +0800

    API Online

--- api/index.py
+++ api/index.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, send_file, request, render_template
+import io
+import gtts
+import pypinyin
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
 
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello")
-    return ("nothing")
+@app.route('/sound')
+def background_process():
+    original = request.args.get("text")
+    new = original[::-1]
+    pyin = pypinyin.core.lazy_pinyin(new)
+    for i in pyin:
+        pyin[pyin.index(i)] = i[::-1]
+    print(pyin)
+    txt = "".join(pyin)
+    sound1 = gtts.gTTS(text=txt, lang='en', slow=False)
+    sound = io.BytesIO()
+    sound1.write_to_fp(sound)
+    sound.seek(0)
+    return send_file(sound, mimetype="audio/wav")
+
+
+if __name__ == '__main__':
+    app.run("127.0.0.1", 2333)
